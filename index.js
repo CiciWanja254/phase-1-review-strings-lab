9e9ed25fc860a26f1d292fdb906f9fdaee2bf363
Cici254 <thukucindy00@gmail.com>
2023-03-15 18:53:11 +0300

    hot fix: change slice last index from 5 to 1

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const currentUser = 'Grace Hopper';
 // const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser.slice(0, 5)}!`;
-console.log(shortGreeting)
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
